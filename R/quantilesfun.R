364ce544bc04846b89ffb4303193a3ab01243654
cphotiadou <christiana.photiadou@knmi.nl>
2017-05-18 14:13:48 +0000

    ignore example (fix later for tavg quantiles)

--- R/quantilesfun.R
+++ R/quantilesfun.R
@@ -53,25 +53,6 @@ get.prec.var.quantiles <- function(filled.prec, date.series, bs.date.range, qtil
 #' @note Units are assumed to be mm/day for precipitation and degrees Celsius
 #' for temperature. No units conversion is performed internally.
 #' 
-#' @examples
-#' library(PCICt)
-#' 
-#' ## Create a climdexInput object from some data already loaded in and
-#' ## ready to go.
-#' 
-#' ## Parse the dates into PCICt.
-#' tmax.dates <- as.PCICt(do.call(paste, ec.1018935.tmax[,c("year",
-#' "jday")]), format="%Y %j", cal="gregorian")
-#' tmin.dates <- as.PCICt(do.call(paste, ec.1018935.tmin[,c("year",
-#' "jday")]), format="%Y %j", cal="gregorian")
-#' prec.dates <- as.PCICt(do.call(paste, ec.1018935.prec[,c("year",
-#' "jday")]), format="%Y %j", cal="gregorian")
-#' 
-#' ## Load the data in.
-#' quantiles <- get.outofbase.quantiles(ec.1018935.tmax$MAX_TEMP,
-#' ec.1018935.tmin$MIN_TEMP, ec.1018935.prec$ONE_DAY_PRECIPITATION,
-#' tmax.dates, tmin.dates, prec.dates, base.range=c(1971, 2000))
-#'
 #' @export
 get.outofbase.quantiles <- function(tmax=NULL, tmin=NULL, tavg=NULL, prec=NULL, tmax.dates=NULL, 
                                     tmin.dates=NULL, tavg.dates=NULL, prec.dates=NULL, base.range=c(1961, 1990), 
